7900e966927fb5f2a0fa0bb5a87d4e03df95a8c7
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2026-02-18 13:46:54 +0900

    chore: change public dir and media dir

--- tina/config.ts
+++ tina/config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "tinacms";
 import collections from "./collection.ts";
 
+const clientId = Deno.env.get("TINA_CLIENT_ID");
+const token = Deno.env.get("TINA_TOKEN");
+
 export default defineConfig({
+  branch: "main",
+  clientId,
+  token,
   media: {
     // If you wanted cloudinary do this
     // loadCustomStore: async () => {
@@ -10,13 +16,13 @@ export default defineConfig({
     // },
     // this is the config for the tina cloud media store
     tina: {
-      publicFolder: "public",
-      mediaRoot: "uploads",
+      publicFolder: "media",
+      mediaRoot: "./",
     },
   },
   build: {
     publicFolder: "public", // The public asset folder for your framework
-    outputFolder: "admin", // within the public folder
+    outputFolder: "./", // within the public folder
   },
   schema: { collections },
 });
