910159dac40f6c330d765e678f4da57114df1991
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2026-02-23 23:37:07 +0900

    feat(scripts): add custom validator for content

---
+++ scripts/validator.ts
@@ -0,0 +1,101 @@
+import {
+  type ReferenceDefinition,
+  type Source,
+  type TinaLock,
+  tinaLockToValidationDefinitions,
+  type ValidationDefinition,
+} from "./schema.ts";
+import tinaLock from "../tina/tina-lock.json" with { type: "json" };
+import { existsSync, expandGlob } from "@std/fs";
+import matter from "gray-matter";
+import ajv from "ajv";
+import { join } from "@std/path";
+
+async function* validate(
+  defifinitions: ValidationDefinition[],
+  options: { exclude?: string[]; rootDir: string },
+): AsyncIterable<Error> {
+  for (const definition of defifinitions) {
+    for (const pattern of definition.source.patterns) {
+      const iterator = expandGlob(pattern, { "exclude": options?.exclude });
+
+      for await (const entry of iterator) {
+        if (entry.isFile) {
+          const content = await Deno.readTextFile(entry.path);
+          const object = convertTo(content, definition.source);
+
+          const c = new ajv.Ajv();
+
+          c.addKeyword({
+            keyword: "reference",
+            async: false,
+            validate: (schema: ReferenceDefinition, data: string): boolean => {
+              switch (schema.type) {
+                case "internal": {
+                  const pathname = join(options.rootDir, schema.base, data);
+
+                  return existsSync(pathname);
+                }
+                case "external": {
+                  throw new Error();
+                }
+              }
+            },
+            errors: true,
+          });
+
+          const valid = c.validate(definition.schema, object);
+
+          if (!valid) {
+            yield new Error(entry.path);
+          }
+        }
+      }
+    }
+  }
+}
+
+function convertTo(content: string, source: Source): object {
+  switch (source.format) {
+    case "markdown": {
+      const { data, content: body } = matter(content);
+
+      return {
+        ...data,
+        body,
+      };
+    }
+    case "json": {
+      return JSON.parse(content);
+    }
+    case "yaml": {
+      throw new Error("unimplemented");
+    }
+  }
+}
+
+if (import.meta.main) {
+  const dirname = import.meta.dirname;
+
+  if (!dirname) {
+    throw new Error("dirname is not defined");
+  }
+
+  const definitions = tinaLockToValidationDefinitions(
+    tinaLock as TinaLock,
+    dirname,
+  );
+  const result = validate(definitions, {
+    exclude: ["**/.gitkeep.*"],
+    rootDir: dirname,
+  });
+
+  // deno-lint-ignore no-top-level-await
+  const errors = await Array.fromAsync(result);
+
+  if (errors.length) {
+    throw new Error();
+  } else {
+    console.info("Ok");
+  }
+}
