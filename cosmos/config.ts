b078859b0272eb196b01ead285cbc86382b371fa
miyauci <git@miyauchi.dev>
2026-04-20 12:36:28 +0000

    refactor: use external field codec

--- cosmos/config.ts
+++ cosmos/config.ts
@@ -1,11 +1,4 @@
-import type {
-  Config,
-  Field,
-  FieldCodec,
-  FieldContext,
-  Node,
-  StructureValue,
-} from "@cosmos/core";
+import type { Config } from "@cosmos/core";
 import { FsStorage } from "@cosmos/storage-fs";
 import { DenoIO } from "@cosmos/storage-fs/deno";
 import { FrontmatterFormatterDefinition } from "@cosmos/formatter-frontmatter";
@@ -13,16 +6,16 @@ import { FsIndexer } from "@cosmos/index-fs";
 import { JsonFormatterDefinition } from "@cosmos/formatter-json";
 import { YamlFormatterDefinition } from "@cosmos/formatter-yaml";
 import { TextFormatterDefinition } from "@cosmos/formatter-text";
-import { AssetFieldCodec } from "@cosmos/field-asset";
-import { StringFieldCodec } from "@cosmos/field-string";
-import { MapField } from "@cosmos/field-map";
-import { BooleanFieldCodec } from "@cosmos/field-boolean";
-import { NumberFieldCodec } from "@cosmos/field-number";
-import { ListField } from "@cosmos/field-list";
-import { InstanceField } from "@cosmos/field-instance";
-import { MarkdownCodec } from "@cosmos/field-markdown";
-import { PathReferenceFieldCodec } from "@cosmos/field-path-reference";
-import { DatetimeFieldCodec } from "@cosmos/field-datetime";
+import { AssetCodec } from "@cosmos/codec-asset";
+import { StringCodec } from "@cosmos/codec-string";
+import { MapField } from "@cosmos/codec-map";
+import { BooleanCodec } from "@cosmos/codec-boolean";
+import { NumberCodec } from "@cosmos/codec-number";
+import { ListField } from "@cosmos/codec-list";
+import { InstanceField } from "@cosmos/codec-instance";
+import { MarkdownCodec } from "@cosmos/codec-markdown";
+import { PathReferenceCodec } from "@cosmos/codec-path-reference";
+import { DatetimeCodec } from "@cosmos/codec-datetime";
 import { UnionField } from "@cosmos/field-union";
 import post from "./models/post.ts";
 import author from "./models/author.ts";
@@ -32,71 +25,10 @@ import home from "./models/home.ts";
 import legalDocument from "./models/legal_document.ts";
 import { fromFileUrl } from "@std/path";
 import { PathResolver } from "@cosmos/resolver-path";
+import { FieldCodec } from "@cosmos/field-codec";
 
 const io = new DenoIO();
 const storage = new FsStorage(io);
-
-type Definition = {
-  stirng: FieldCodec;
-  number: FieldCodec;
-  boolean: FieldCodec;
-  map: FieldCodec;
-  instance: FieldCodec;
-  reference: FieldCodec;
-  list: FieldCodec;
-  datetime: FieldCodec;
-  markdown: FieldCodec;
-  asset: FieldCodec;
-  union: FieldCodec;
-};
-
-class Def implements FieldCodec {
-  constructor(private def: Definition) {}
-  parse(structure: StructureValue, field: Field, ctx: FieldContext) {
-    switch (field.type) {
-      case "string": {
-        return this.def.stirng.parse(structure, field, ctx);
-      }
-      case "number": {
-        return this.def.number.parse(structure, field, ctx);
-      }
-      case "boolean": {
-        return this.def.boolean.parse(structure, field, ctx);
-      }
-      case "instance": {
-        return this.def.instance.parse(structure, field, ctx);
-      }
-      case "reference": {
-        return this.def.reference.parse(structure, field, ctx);
-      }
-      case "list": {
-        return this.def.list.parse(structure, field, ctx);
-      }
-      case "asset": {
-        return this.def.asset.parse(structure, field, ctx);
-      }
-      case "map": {
-        return this.def.map.parse(structure, field, ctx);
-      }
-      case "datetime": {
-        return this.def.datetime.parse(structure, field, ctx);
-      }
-      case "markdown": {
-        return this.def.markdown.parse(structure, field, ctx);
-      }
-      case "union": {
-        return this.def.union.parse(structure, field, ctx);
-      }
-    }
-  }
-  stringify(
-    node: Node,
-    field: Field,
-  ): StructureValue | Promise<StructureValue> {
-    return "";
-  }
-}
-
 const file = import.meta.resolve("../");
 const rootDir = fromFileUrl(file);
 
@@ -116,17 +48,17 @@ export default {
     new YamlFormatterDefinition(),
     new TextFormatterDefinition(),
   ],
-  field: new Def({
-    stirng: new StringFieldCodec(),
-    asset: new AssetFieldCodec(rootDir),
+  field: new FieldCodec({
+    string: new StringCodec(),
+    asset: new AssetCodec(rootDir),
     map: new MapField(),
-    boolean: new BooleanFieldCodec(),
-    number: new NumberFieldCodec(),
+    boolean: new BooleanCodec(),
+    number: new NumberCodec(),
     instance: new InstanceField(),
     list: new ListField(),
     markdown: new MarkdownCodec(),
-    reference: new PathReferenceFieldCodec(rootDir),
-    datetime: new DatetimeFieldCodec(),
+    reference: new PathReferenceCodec(rootDir),
+    datetime: new DatetimeCodec(),
     union: new UnionField(),
   }),
   indexes: [
